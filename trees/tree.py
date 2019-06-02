012cd3874e076673c36842541495377c9035888a
Fred Young <fredgyoung@gmail.com>
2019-06-01 18:26:44 -0700

    Added tree.py

---
+++ trees/tree.py
@@ -0,0 +1,56 @@
+from queue import Queue
+
+
+class Node:
+
+    def __init__(self, data=None):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class Tree:
+
+    def __init__(self, node):
+        self.root = node
+
+    def insert_element(self, value):
+        queue = Queue()
+        queue.put(self.root)
+        while not queue.empty():
+            node = queue.get()
+            if node.left:
+                queue.put(node.left)
+            else:
+                node.left = Node(value)
+                return
+            if node.right:
+                queue.put(node.right)
+            else:
+                node.right = Node(value)
+                return
+
+    def level_order_traversal(self):
+        result = []
+        queue = Queue()
+        queue.put(self.root)
+        while not queue.empty():
+            node = queue.get()
+            result.append(node.data)
+            if node.left:
+                queue.put(node.left)
+            if node.right:
+                queue.put(node.right)
+        print(result)
+
+
+if __name__ == '__main__':
+
+    tree = Tree(Node(1))
+    tree.insert_element(2)
+    tree.insert_element(3)
+    tree.insert_element(4)
+    tree.insert_element(5)
+    tree.insert_element(6)
+    tree.insert_element(7)
+    tree.level_order_traversal()
